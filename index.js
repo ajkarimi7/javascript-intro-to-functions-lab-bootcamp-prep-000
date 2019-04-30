cb9fb81f8dff4de269c7618f9beeb7c64373fbfd
ajkarimi7 <ajkarimi7@gmail.com>
2019-04-30 18:51:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
   return "YES INDEED!" }
   
-  else {
+  if (string === "I love you, Grandma") {
     return "I love you, too."}
 }
